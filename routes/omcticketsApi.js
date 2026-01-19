2d35a89e5d7b9a6b9a5dea70dd0805199dcd51e6
wassim-ahmad <wassim@parkonic.ae>
2026-01-19 17:30:58 +0400

    return to production parkonic api

--- routes/omcticketsApi.js
+++ routes/omcticketsApi.js
@@ -528,8 +528,8 @@ router.post('/submit-omc-ticket/:id', upload.none(), verifyToken, requirePermiss
         out_images
       };
 
-      const response = await axios.post('https://dev.parkonic.com/api/street-parking/v2/new-trip', payload, { headers: { 'Content-Type': 'application/json' }, timeout: 10000 });
-      // const response = await axios.post('https://api.parkonic.com/api/street-parking/v2/new-trip', payload, { headers: { 'Content-Type': 'application/json' }, timeout: 10000 });
+      // const response = await axios.post('https://dev.parkonic.com/api/street-parking/v2/new-trip', payload, { headers: { 'Content-Type': 'application/json' }, timeout: 10000 });
+      const response = await axios.post('https://api.parkonic.com/api/street-parking/v2/new-trip', payload, { headers: { 'Content-Type': 'application/json' }, timeout: 10000 });
 
       if (response.data.status === false){
         logger.error('OMC Ticket submission failed immigration', { admin: req.user, response:response.data });
